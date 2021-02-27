5a59097ab6979b6756626da840d723104880da93
Edwards Moses <edwardsmoses3@gmail.com>
2021-02-27 11:45:26 +0100

    https://itnext.io/useful-reduce-use-cases-91a86ee10bcd
    Example Uses of Piping:- Calculating Tthe checkout total for an online store:

--- piping.js
+++ piping.js
@@ -1,11 +1,14 @@
 console.log("result");
 
+//Piping Functions using Reduce
 const pipe = (...functions) => m => functions.reduce((prev, curr) => curr(prev), m);
 
+//Functions to Pipe. 
 const multiTwo = (number) => number * 2;
 const multiThree = (number) => number * 3;
 const multiFour = (number) => number * 4;
 const multiFive = (number) => number * 5;
+
 
 const allMultiplications = pipe(multiTwo, multiThree, multiFour, multiFive);
 
